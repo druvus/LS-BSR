3f5745a5e22e4e8c59c065702d338411c1ff512c
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-09-18 10:07:29 +0200

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup, find_packages
 
 __author__ = "Jason Sahl"
 __credits__ = ["Jason Sahl"]
@@ -8,6 +9,7 @@ __version__ = "1.0"
 __maintainer__ = "Jason Sahl"
 __email__ = "jsahl@tgen.org"
 __status__ = "Development"
+__url__="https://github.com/jasonsahl/LS-BSR"
  
 long_description = """Large scale blast score ratio (LS-BSR)
 LS-BSR is a method to compare open reading frames betweeen
@@ -19,10 +21,12 @@ between groups
 setup(name='LS-BSR',
       version=__version__,
       description='Large scale blast score ratio',
+      url=__url__,
       author=__maintainer__,
       author_email=__email__,
       maintainer=__maintainer__,
       maintainer_email=__email__,
-      packages=['ls_bsr'],
+      packages=find_packages(),
+      scripts = ['ls_bsr.py'],
       long_description=long_description
 )
